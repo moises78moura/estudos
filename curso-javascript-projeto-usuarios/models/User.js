385d2a995e3abe59abefa94b3cfc648a26b5bb30
moises78moura <moisesicdemoura@gmail.com>
2019-04-12 16:40:08 -0300

    Update User.js

--- curso-javascript-projeto-usuarios/models/User.js
+++ curso-javascript-projeto-usuarios/models/User.js
@@ -53,5 +53,18 @@ class User{
         this._photo = value;
     }
 
+    loadFromJSON(json){
+
+        for(let name in json){
+            switch(name){
+                case '_register':
+                    this[name] = new Date(json[name]);
+                    break;
+                default:
+                    this[name] = json[name];
+            }
+        }
+    }
+
 
 }
