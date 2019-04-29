565adf4c7b77575be154f1f8efe429fe064e3c3f
moises78moura <moisesicdemoura@gmail.com>
2019-04-29 17:44:34 -0300

    Update users.js
    Continuação

--- restfull/routes/users.js
+++ restfull/routes/users.js
@@ -8,26 +8,99 @@
 // let express = require('express');//carrega o modulo http
 // let routes  = express.Router();
 
+let NeDB = require('nedb');
+
+let db = new NeDB({
+    filename:'users.db',
+    autoload:true
+});
+
+
 module.exports = (app) =>{
-    app.get('/users', (req, res) => {
+
+
+    let route = app.route('/users');
+
+    route.get((req, res) => {
+    // app.get('/users', (req, res) => {
 
         console.log("URL -> ", req.url);
         console.log("METHOD -> ", req.method);
+        db.find({}).sort({name:1}).exec((err, users) => {
+            if(err){
+             app.utils.error.send(err, req, res);
+            }else{
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({users});// no ecma06 quando se tem uma chave 'users' que o mesmo nome da variável pode usar somente a chave 
+                // res.json({users:users }); de acordo com a descrição acima do ECMA06 a expressão acima é equivalente a essa aqui.
+            }
+        });//busca os dados no banco, e ordena por 'name/nome' em ordem crescente, -1 seria decrescente
+       
+    });
+    
+    route.post((req, res) => {
+
+       if(!app.utils.validator.user(app, req, res)) return false;
+
+    // app.post('/users', (req, res) => {
+        //res.json(req.body);
+        db.insert(req.body,(err,user) =>{
+            if(err){
+                app.utils.error.send(err, req, res);
+                // console.log('error: ${err}');
+                // res.status(400).json({error:err});
+            }else{
+                res.status(200).json(user);
+            }
+        });
+    });
+
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+                // console.log('error: ${err}');
+                // res.status(400).json({error:err});
+            }else{
+                res.status(200).json(user);
+            }
+        });
+    });
+
+    routeId.put((req, res) => {
+
+        if(!app.utils.validator.user(app, req, res)) return false;
         
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({users:[{
-                        name:'Moises', 
-                        profile:'Usuário', 
-                        email:'moises@ig.com', 
-                        id:1
-                    }]
-                });
+        db.update({_id:req.params.id}, req.body, err => {
+            if(err){
+                app.utils.error.send(err, req, res);
+                // console.log('error: ${err}');
+                // res.status(400).json({error:err});
+            }else{
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        });
     });
-    
-    app.post('/users', (req, res) => {
-    
-        res.json(req.body);
+
+    routeId.delete((req, res) => {
+
+        db.remove({_id:req.params.id}, {}, err =>{
+            if(err){
+                app.utils.error.send(err, req, res);
+                // console.log('error: ${err}');
+                // res.status(400).json({error:err});
+            }else{
+                res.status(200).json(req.params);
+            }
+        } );
+        
     });
+
+
+
 };
 // module.exports = routes;
